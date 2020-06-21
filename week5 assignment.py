8460d8a964a687f0f010223c3e35d8533fb8d275
habybheart1 <55719586+habybheart1@users.noreply.github.com>
2020-06-21 09:14:08 +0100

    Add files via upload

---
+++ week5 assignment.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#akintobi abibat
+#akintobiabibat@yahoo.com
+olamide = {"name": "Abibat", "best_color" : ["blue", "green", "yellow"], "address": "Oluwalogbon"}
+print(olamide["best_color"])
+           
+ade = ["blue", "green", "yellow"]
+print(ade)
+del(olamide["name"])
+print(olamide)
+olamide["food"] = "rice"
+print(olamide)
+
+
+# In[ ]:
+
+
+
+
